0af5de96c812eb911487f3279518be7fef3d71a8
Ritwikcp-ctrl <su30rai213@gmail.com>
2026-07-26 23:11:15 +0530

    data in the queue but not processing

---
+++ src/queue.js
@@ -0,0 +1,19 @@
+class Queue {
+  constructor() {
+    this.items = [];
+  }
+
+  enqueue(item) {
+    this.items.push(item);
+  }
+
+  dequeue() {
+    return this.items.shift();
+  }
+
+  size() {
+    return this.items.length;
+  }
+}
+
+export default Queue;
