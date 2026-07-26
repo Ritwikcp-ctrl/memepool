07b35a8bc8f07ebf8d4282b8701c0c4902341fe5
Ritwikcp-ctrl <su30rai213@gmail.com>
2026-07-26 23:12:03 +0530

    insted of console queued the Trade

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import PriceWatcherClient from './ws-client.js';
 import EventBus from './event-bus.js';
+import Queue from "./queue.js";
 
 const client = new PriceWatcherClient("wss://stream.binance.com:9443/ws/btcusdt@trade");
 const bus = new EventBus()
+const queue = new Queue()
 
 client.bus.on('trade', (trade) => {
-  console.log(trade);
+  queue.enqueue(trade); //This helps in the putting them into the line and not for precessing 
+
+  setInterval(() => console.log("queue size:", queue.size), 1000);
 });
